5eb2cdd0a90588f32c502783336f0c3d0c988d97
zhongfanggui <1031557353@qq.com>
2023-05-24 23:26:22 +0800

    Add file header

--- app/src/main/java/com/zfg/mediafun/MainActivity.java
+++ app/src/main/java/com/zfg/mediafun/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author zhongfanggui
+ * @version 1.0.0
+ * @since 2023/5/24
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
